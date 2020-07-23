ffc4af8fbe69bad66a38ab73834182aa682416b0
Jonathan Dillabough <JDillabough87@gmail.com>
2020-07-23 12:38:26 -0500

    Fixed styling issues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { MenuItem, FormControl, Select } from "@material-ui/core";
+import { MenuItem, FormControl, Select, Card } from "@material-ui/core";
+
+import InfoBox from "./components/InfoBox";
+import Map from "./components/Map";
+
 import "./App.css";
 
 function App() {
-  const [ countries, setCountries ] = useState([]);
-  const [ country, setCountry ] = useState('worldwide');
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -23,24 +27,36 @@ function App() {
     getCountriesData();
   }, []);
 
-  const onCountryChange = async (e) => {
+  const onCountryChange = async e => {
     const countryCode = e.target.value;
-
     setCountry(countryCode);
-  }
+  };
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>Cov-19 Tracker</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" onChange={onCountryChange} value={country}>
-            <MenuItem value='worldwide'>Worldwide</MenuItem>
-            {countries.map(country => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      <div className="app__fragmemt__left">
+        <div className="app__header">
+          <h1>Cov-19 Tracker</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={123} total={1000} />
+          <InfoBox title="Recovered" cases={5000} total={10000} />
+          <InfoBox title="Deaths" cases={43223} total={453278} />
+        </div>
+
+        <Map />
       </div>
     </div>
   );
